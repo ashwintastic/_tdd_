50d902656f365b3a0e6d2c8b75d46c463b4f8b71
Kumar <Ashwini.Kumar@msci.com>
2025-05-25 21:32:52 +0530

    added more test

--- calculator.rb
+++ calculator.rb
@@ -3,8 +3,21 @@ class Calculator
      if input.strip.empty?
        result =  0
      else
-       input = input.split(',').map(&:to_i)
-       result = input.sum
+       delimiter_regex = /,|\n/
+       if input.start_with?("//")
+         delimiter_header, input = input.split("\n", 2)
+         custom_delimiters = delimiter_header[2..-1]
+
+         # Extract multiple or long delimiters
+         delimiters = custom_delimiters.scan(/\[(.*?)\]/).flatten
+         if delimiters.empty?
+           delimiters = [custom_delimiters]
+         end
+         delimiter_regex = Regexp.union(delimiters.map { |d| Regexp.escape(d) })
+       end
+
+       numbers = input.split(delimiter_regex).map(&:to_i)
+       result = numbers.sum
      end
      result
   end
